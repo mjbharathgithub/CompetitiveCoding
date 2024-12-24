753ef443f50566824a766576730f6c658e071acd
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-12-24 22:34:19 +0530

    Create Recursion.java

---
+++ techniques/Recursion.java
@@ -0,0 +1,50 @@
+
+public class Main
+{
+        static int count=0;
+    static int nonNegSum(int n){
+        if(n>0){
+            System.out.print(n+" ");
+            return n+nonNegSum(n-1);
+        }
+        return 0;
+    }
+    
+
+    static void countGridPaths(int row,int col){
+        if(row<1||col<1){
+            return;
+        }
+        else{
+            if(row==1&&col==1) count++;
+            countGridPaths(row-1,col);
+            countGridPaths(row,col-1);
+        }
+        
+    }
+    
+    static int countGridPathsInt(int row,int col){
+        if(row==1||col==1){
+            return 1;
+        }
+        else{
+            return countGridPathsInt(row-1,col) + countGridPathsInt(row,col-1);
+        }
+        
+    }
+	public static void main(String[] args) {
+	    
+	    countGridPaths(41,4);
+	    int another=countGridPathsInt(41,4);
+	    System.out.println(count);
+	    System.out.println(another);
+	    
+	    
+	    
+	    
+	    
+	   // int sum=nonNegSum(3);
+	//	System.out.println("\n"+sum);
+	//	System.out.print("asdfasdf");
+	}
+}
