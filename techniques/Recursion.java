a56135d7226e1be382b9071c57d29938dcfcffb8
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-12-28 23:35:29 +0530

    Update Recursion.java

--- techniques/Recursion.java
+++ techniques/Recursion.java
@@ -40,6 +40,53 @@ public class Main
             return Integer.parseInt(Integer.toString(number % 10) + reverseNumber(number / 10));
         }
     }
+	public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    // 2. Fibonacci
+    public static int fibonacci(int n) {
+        if(n<=1){
+            return n;
+        }else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    // 3. Power of a Number
+    public static double power(double x, int n) {
+        if(n==0){
+            return 1;
+        } else if(n>0){
+            return x * power(x, n - 1);
+        }else{
+            return 1 / power(x, -n); // Handle negative exponents
+        }
+    }
+
+    // 4. Greatest Common Divisor (GCD)
+    public static int gcd(int a, int b) {
+        if(b==0){
+            return a;
+        }else{
+            return gcd(b, a % b);
+        }
+    }
+
+    // 5. Tower of Hanoi (Illustrative - Prints moves)
+    public static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+        if(n==1){
+            System.out.println("Move disk 1 from rod " + from_rod + " to rod " + to_rod);
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
 	public static void main(String[] args) {
 	    
 	    countGridPaths(41,4);
