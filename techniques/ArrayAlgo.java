8437170cc39b10dfa18ea76351f45d23590d73e6
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-03-15 23:20:51 +0530

    Update ArrayAlgo.java

--- techniques/ArrayAlgo.java
+++ techniques/ArrayAlgo.java
@@ -57,5 +57,40 @@ static void swap(int arr[],int n1,int n2){
         }
     }
 
+    public static void rotateReversal(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        if (k < 0) {
+            k += n;
+        }
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+ 
+    public static void rotateTempArray(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        if (k < 0) {
+            k += n;
+        }
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(temp, 0, nums, 0, n);
+    }
+
     
 }
